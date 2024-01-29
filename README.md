d2f83099bb9b30ba91c82b10fcafe9353f58e984
Fernando Barroso <fernandobarroso@suaquadra.com.br>
2024-01-28 17:06:21 -0700

    Pacmen Exercise

---
+++ README.md
@@ -0,0 +1,15 @@
+# PacMan Game
+
+## Description
+A simple PacMan game built using HTML, CSS, and JavaScript. You can add PacMen, start and pause the game.
+
+## How to Run
+1. Clone the repository.
+2. Open `index.html` in a web browser.
+3. Click "Add PacMan" to create as many PacMen as you want.
+4. Click "Start Game" to start the game or "Pause Game" to pause it.
+
+## Roadmap of Future Improvements
+- Add more styles to the page for a better gaming experience.
+- Include images of PacMen facing up and down, allowing them to move horizontally or vertically one at a time.
+- Introduce obstacles on the map that randomly point left, right, up, or down. PacMen colliding with obstacles will change their direction accordinly.
---
+++ pacmen.js
@@ -0,0 +1,101 @@
+var pos = 0;
+const pacArray = [
+    ['PacMan1.png', 'PacMan2.png'],
+    ['PacMan3.png', 'PacMan4.png']
+];
+
+var active = false;
+var interval = null;
+var pacMen = [];
+
+function setToRandom(scale) {
+    return {
+        x: Math.floor(Math.random() * scale),
+        y: Math.floor(Math.random() * scale)
+    }
+}
+
+function makePac() {
+    let pageWidth = window.innerWidth;
+    let pageHeight = window.innerHeight;
+    let velocity = setToRandom(30);
+    velocity.x += 10;
+    velocity.y += 10;
+    let position = setToRandom(Math.min(pageWidth, pageHeight));
+    let imgStyle = 0;
+    let direction = 0;
+    let game = document.getElementById('game');
+    let element = document.createElement('img');
+    element.style.position = 'absolute';
+    element.src = 'PacMan1.png';
+    element.width = 100;
+    element.style.left = position.x;
+    element.style.top = position.y;
+
+    game.appendChild(element);
+    return {
+        position,
+        velocity,
+        element,
+        imgStyle,
+        direction,
+    }
+}
+
+function makeOne() {
+    pacMen.push(makePac())
+}
+
+function run() {                
+    pacMen.forEach((item) => {
+        checkPageBounds(item)
+        item.imgStyle = (item.imgStyle + 1) % 2;
+        item.element.src = pacArray[item.direction][item.imgStyle];
+        item.position.x += item.velocity.x;
+        item.position.y += item.velocity.y;
+        item.element.style.left = item.position.x + "px";
+        item.element.style.top = item.position.y + "px";
+    })
+}
+
+function checkPageBounds(item) {
+    let pageWidth = window.innerWidth;
+    let pageHeight = window.innerHeight;
+    let imgWidth = item.element.width;
+    let imgHeight = item.element.height;
+    if (item.position.x >= pageWidth - imgWidth - item.velocity.x) {
+        item.direction = 1
+        item.velocity.x = -item.velocity.x
+        item.position.x = pageWidth - imgWidth
+    }
+    if (item.position.x < -item.velocity.x) {
+        item.direction = 0
+        item.velocity.x = -item.velocity.x
+        item.position.x = 0
+    }
+
+    if (item.position.y >= pageHeight - imgHeight - item.velocity.y) {
+        item.velocity.y = -item.velocity.y
+        item.position.y = pageHeight - imgHeight
+    }
+    if (item.position.y < -item.velocity.y) {
+        item.velocity.y = -item.velocity.y
+        item.position.y = 0
+    }
+}
+
+function checkRun() {
+    let button = document.getElementById("startBtn")
+    if (active === true) {
+        active = false
+        button.innerHTML = "Start Game"
+        button.style.backgroundColor = "green"
+        clearInterval(interval)
+    } else {
+        active = true
+        button.innerHTML = "Pause Game"
+        button.style.backgroundColor = "red"
+        interval = setInterval(run, 100)
+        run()
+    }
+}
